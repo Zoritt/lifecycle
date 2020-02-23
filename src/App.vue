f1adbfdba600e70938624cf17a620687068aedf5
Paweł Życiński <zoritt@gmail.com>
2020-02-23 16:29:26 +0100

    lifecycle cz. 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-primary text-white m-2 p-2">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" v-model="checked" />
+      <label>Pole checkbox</label>
+    </div>
+    Stan zaznaczenia: {{ checked }}
+    <div class="bg-info p-2">
+      Imiona:
+      <ul>
+        <li v-for="name in names" v-bind:key="name">{{ name }}</li>
+      </ul>
+    </div>
+    <div class="text-white center my-2">
+      <button class="btn btn-light" v-on:click="doChange">Zmień</button>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Vue from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  name: "App",
+  data: function() {
+    return {
+      checked: true,
+      names: []
+    };
+  },
+  beforeCreate() {
+    console.log("Wywołano metodę beforeCreate" + this.checked);
+  },
+  created() {
+    console.log("Wywołano metodę created" + this.checked);
+  },
+  mounted() {
+    this.$el.dataset.names.split(",").forEach(name => this.names.push(name));
+  },
+  beforeUpdate() {
+    console.log(
+      `Wywołano metodę beforeUpdate. Stan zaznaczenia: ${this.checked}` +
+        ` Imię: ${this.names[0]} Elementy listy: ` +
+        this.$el.getElementsByTagName("li").length
+    );
+  },
+  updated() {
+    console.log(
+      `Wywołano metodę updated. Stan zaznaczenia: ${this.checked}` +
+        ` Imię: ${this.names[0]} Elementy listy: ` +
+        this.$el.getElementsByTagName("li").length
+    );
+  },
+  methods: {
+    doChange() {
+      this.checked = !this.checked;
+      this.names.reverse();
+      Vue.nextTick(() => console.log("Wykonano wywołanie zwrotne"));
+    }    
+  },
+  watch: {
+      checked: function(newValue, oldValue) {
+        console.log(`Sprawdzono obserwatora, Stara wartość: ${oldValue}, Nowa wartość: ${newValue}`);
+      }
+    }
+};
+</script>                             
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 Vue.config.productionTip = false
 
+Vue.config.productionTip = false
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    components: { App },
+    template: '<App data-names="Janek, Alicja, Piotrek, Daria" />'
+})
